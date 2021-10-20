fc14bb8b518b26971cb59b590016e80975e3cb78
Nasian <87439662+nasiana@users.noreply.github.com>
2021-10-20 16:16:05 +0100

    Add files via upload

--- Lecture_rewatch.py
+++ Lecture_rewatch.py
@@ -23,6 +23,17 @@ print(file.readline())
 # remember to always close the file or it can close many errors
 file.close()
 
+'''
+# if a file exists already:
+# w+ will override it (truncate it to length 0 - i.e. chop off the bits at the end until the length of the file is 0)
+# r+ will not override the contents of the file so you can still read it
+'''
+'''
+if a file does not exist:
+# w+ will create a new file
+# r+ will throw an error
+'''
+
 # experimenting with r+
 file = open('test_file.txt', 'r+')
 file.write("Now I have written over using r+")
@@ -56,5 +67,60 @@ file = open('my_new_file_for_writing.txt', 'a')  # NB 'a' mode for append
 file.writelines(movie_quotes)
 file.close()
 
+# CONTEXT MANAGER
 
-# stopped at the 28 minute mark
+'''
+Use the with statement to shorten code and make it more concise when file handling. Use with means that you/
+don't need to add file.close() at the end of code. The file is automatically closed when executing 'with'.
+'''
+
+with open('people.txt', 'w+') as text_file:
+    people = 'Nasian \n Maja \n Lana'
+    text_file.write(people)
+
+# alternative way to write it without the context manager
+
+file = open('people.txt', 'w+')
+try:
+    file.write('Nasian \n Marwan \n Sara')
+finally:
+    file.close()
+
+# reading
+'''
+Interesting behaviour, as I have previousled aliased using 'with open('people.txt', 'w+') as text_file', it seems/
+that the following code will only work when I have 'as text_file' in the code. Without it I get
+'''
+with open('people.txt', 'r') as text_file:
+    contents = text_file.read()
+    print(contents)
+
+
+#
+equivalent
+
+file = open('people.txt', 'r')
+try:
+    print(file.read())
+finally:
+    file.close()
+
+'''
+Create a to-do list program that writes user input to a file
+The program should:
+- Ask the user to input a new to-do item
+- Read the contents of the existing to-do items
+- Add the new to do item to the existing to-do items
+- Save the updated to-do items
+Optional: You can create the new file either manually or using Python.
+'''
+
+# to-do programme
+user = input('Input a new to-do item: ')
+
+with open('to-do_items.txt', 'r') as ex1_file:
+    contents_ex1 = ex1_file.read()
+    print(contents_ex1)
+
+with open('to-do_items.txt', 'a') as ex1_file:
+    ex1_file.write(user)
